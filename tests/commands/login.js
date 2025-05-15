5c3f6f9b1eea7149333f49b20abde27682c79864
Imran Khan <usimran2000@yahoo.com>
2025-05-15 16:36:49 -0500

    demo

---
+++ tests/commands/login.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+async function testLogin(page){
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="title"]')).toBeVisible();
+}
+
+module.exports = {
+  testLogin
+
+}
